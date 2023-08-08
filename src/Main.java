86003e691f061624a6896157a6052dc62b63bc37
eosfelipe <escobedo.felipe@hotmail.com>
2023-08-07 19:55:52 -0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void print(String text){
+    public static void print(String text) {
         System.out.println(text);
     }
+
     public static void main(String[] args) {
 
         String value = "";
@@ -15,24 +16,43 @@ public class Main {
         HashTable h = new HashTable(10);
         h.fill();
 
-        print("*********Agenda UNIR*********");
-        print("Deseas agregar un contacto nuevo? SI(S) / NO(N)");
-        value = in.nextLine();
+        // agregar elementos de ejemplo
+        h.add(new Contact("Marcos", "9999123456"));
+        h.add(new Contact("Carlos", "9999123456"));
+        h.add(new Contact("Felipe", "9999123456"));
+        h.add(new Contact("Luiss", "9999123456"));
+        h.add(new Contact("Cristina", "9999123456"));
+        h.add(new Contact("Yadira", "9999123456"));
+        h.add(new Contact("Lulu", "9999123456"));
+        h.add(new Contact("Lule", "9999123456"));
 
-        if(value.equalsIgnoreCase("s")){
-            print("Nombre: ");
-            String name = in.nextLine();
-            print("Teléfono: ");
-            String phone = in.nextLine();
-            print("Correo electronico: ");
-            String email = in.nextLine();
-            Contact c = new Contact(name, phone, email);
-            h.add(c);
-            print("\n");
-            if(h.validateFactor()){
-                print("WARNING!!!! El factor de carga supera el 75% de la capacidad");
-            }
+        if (h.validateFactor()) {
+            print("WARNING!!!! El factor de carga supera el 75% de la capacidad");
             h.viewContent();
-        } else return;
+        } else {
+            print("OK");
+            h.viewContent();
+        }
+
+        // print("*********Agenda UNIR*********");
+        // print("Deseas agregar un contacto nuevo? SI(S) / NO(N)");
+        // value = in.nextLine();
+
+        // if (value.equalsIgnoreCase("s")) {
+        // print("Nombre: ");
+        // String name = in.nextLine();
+        // print("Teléfono: ");
+        // String phone = in.nextLine();
+        // print("Correo electronico: ");
+        // String email = in.nextLine();
+        // Contact c = new Contact(name, phone, email);
+        // h.add(c);
+        // print("\n");
+        // if (h.validateFactor()) {
+        // print("WARNING!!!! El factor de carga supera el 75% de la capacidad");
+        // }
+        // h.viewContent();
+        // } else
+        // return;
     }
 }
--- src/hashtable/HashTable.java
+++ src/hashtable/HashTable.java
@@ -6,70 +6,78 @@ public class HashTable {
     private ArrayList<Contact> phoneBook;
     private int capacity;
 
-
-    public HashTable(int capacity){
+    public HashTable(int capacity) {
         this.phoneBook = new ArrayList<>(capacity);
-        this.capacity = capacity -1;
+        this.capacity = capacity;
     }
 
     public ArrayList<Contact> getPhoneBook() {
         return phoneBook;
     }
 
-    public void add(Contact c){
+    public void add(Contact c) {
         int index = this.generateHash(c.getName());
-        this.phoneBook.add(index,c);
-    }
-
-    //TODO
-    public void delete(){
-
-    }
-
-    public Contact getElement(String name){
-        int hashCode = this.generateHash(name);
-        return this.phoneBook.get(hashCode);
-    }
-
-    private int generateHash(String value){
-        int sumValue = 0;
-        for(int i = 0; i < value.length(); i++){
-            sumValue += value.toLowerCase().charAt(i) * i;
-            //System.out.println("Para "+value.charAt(i)+" ascii value: "+(int) value.charAt(i));
+        if (this.phoneBook.get(index) != null) {
+            System.out.println("index ocupado colision");
+        } else {
+            this.phoneBook.set(index, c);
         }
-        //System.out.println("suma: "+sumValue+" modulo: "+sumValue%10);
-        //System.out.println("size: "+this.phoneBook.size());
+
+    }
+
+    // TODO
+    public void delete() {
+
+    }
+
+    public Contact getElement(String name) {
+        int index = this.generateHash(name);
+        return this.phoneBook.get(index);
+    }
+
+    private int generateHash(String value) {
+        int sumValue = 0;
+        for (int i = 0; i < value.length(); i++) {
+            sumValue += value.toLowerCase().charAt(i) * i;
+            // System.out.println(
+            // "Para " + value.toLowerCase().charAt(i) + " ascii value: " + (int)
+            // value.toLowerCase().charAt(i));
+        }
+        // System.out.println("suma: " + sumValue + " modulo: " + sumValue %
+        // this.capacity);
+        // System.out.println("size: "+this.phoneBook.size());
 
         return (int) sumValue % this.capacity;
     }
 
-    public boolean validateFactor(){
+    public boolean validateFactor() {
         int elements = 0;
-        for(int i = 0; i < this.phoneBook.size(); i++){
-            if(this.phoneBook.get(i) != null){
+        for (int i = 0; i < this.phoneBook.size(); i++) {
+            if (this.phoneBook.get(i) != null) {
                 elements++;
             }
         }
-        //System.out.println((1.0 * elements) / this.capacity);
+        // System.out.println((1.0 * elements) / this.capacity);
         return (1.0 * elements) / this.capacity >= 0.75;
     }
 
-    public void viewContent(){
+    public void viewContent() {
+        System.out.println("Tamaño de la agenda: " + this.phoneBook.size());
         for (int i = 0; i < this.phoneBook.size(); i++) {
-            if(this.phoneBook.get(i) != null){
-                System.out.println(this.phoneBook.get(i).getName());
-            }else {
-                System.out.println(this.phoneBook.get(i));
+            if (this.phoneBook.get(i) != null) {
+                System.out.println(i + " " + this.phoneBook.get(i).getName());
+            } else {
+                System.out.println(i + " " + this.phoneBook.get(i));
             }
         }
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return this.phoneBook.size() == 0;
     }
 
-    public void fill(){
-        for (int i = 0; i < this.capacity; i++){
+    public void fill() {
+        for (int i = 0; i < this.capacity; i++) {
             this.phoneBook.add(null);
         }
     }
